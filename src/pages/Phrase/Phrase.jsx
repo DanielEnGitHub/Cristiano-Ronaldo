c2753a7448c057950640512bb5940e92dfdd0720
Daniel <danieljgallina@gmail.com>
2023-05-19 15:26:10 -0600

    add: border animate

--- src/pages/Phrase/Phrase.jsx
+++ src/pages/Phrase/Phrase.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { ronaldoJV } from "../../utils/image";
 
 const Phrase = () => {
-  return <div>Phrase</div>;
+  return (
+    <>
+      {/* <div className="main">
+        <div className="imagesRonaldo">
+          <img src={ronaldoJV} alt="ronaldojv.png" />
+        </div>
+      </div> */}
+
+      <div className="main">
+        <div className="imagesRonaldo">
+          {/* <img src={ronaldoJV} alt="ronaldojv.png" /> */}
+        </div>
+        {/* <img src={ronaldoJV} className="hidden" /> */}
+      </div>
+
+      {/* <img src={ronaldoJV} alt="ronaldojv.png" /> */}
+      {/* <span className="imagesRonaldo">HOLA</span> */}
+    </>
+  );
 };
 
 export default Phrase;
