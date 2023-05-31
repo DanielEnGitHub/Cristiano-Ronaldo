b64334c64c6d192190c5be56633ceb2eaab1a973
Daniel <danieljgallina@gmail.com>
2023-05-31 11:47:41 -0600

    add responsive to home and validation firefox

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { cr7PNG } from "../../utils/image";
 import "./home.css";
 import YouTubePlayer from "./YouTubePlayer";
 
 const Home = () => {
+  const isFirefox = navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
   return (
     <>
       <div className="main-home">
+        {isFirefox ? (
+          <div className="main-home-image">
+            <img src={cr7PNG} className="image-w" />
+          </div>
+        ) : (
+          <div className="bg-image">
+            <YouTubePlayer videoID="W9ABYUvkF2I" />
+          </div>
+        )}
+
         {/* W9ABYUvkF2I  cool */}
         {/* eFgSL_CHYwg  goals */}
         {/* NSUPwdS9sIo funny */}
-        <div className="bg-image">
-          <YouTubePlayer videoID="NSUPwdS9sIo" />
-        </div>
       </div>
     </>
   );
