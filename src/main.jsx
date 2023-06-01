14820c2bf28241420a89711b3c329eebbf8cba39
Daniel <danieljgallina@gmail.com>
2023-06-01 17:10:25 -0600

    add base content

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./assets/css/main.css";
+import "./assets/css/grid.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/utils/image.js
+++ src/utils/image.js
@@ -1,4 +1,4 @@
-import ronaldoJV from "../assets/images/ronaldojv.png";
+import ronaldoJV from "../assets/images/ronaldomd.png";
 import cr7PNG from "../assets/images/cr7.png";
 import cr7SVG from "../assets/images/cr7svg.svg";
 
