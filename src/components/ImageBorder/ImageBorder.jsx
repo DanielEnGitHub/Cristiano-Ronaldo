48029276579c92f6608c1a325f2dece85d09e852
Daniel <danieljgallina@gmail.com>
2023-06-07 17:27:51 -0600

    fix images

--- src/components/ImageBorder/ImageBorder.jsx
+++ src/components/ImageBorder/ImageBorder.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const ImageBorder = ({ imageURL, revert = false }) => {
   return (
-    <div className={`card-2 ${revert && "card-2-revert"}`}>
+    <div className={`card-2 ${revert ? "card-2-revert" : ""}`} style={{position: "relative"}}>
       <div className="principal">
-        <div className="container">
+        <div className="container" >
           <div className="main" style={{ "--image-url": `url(${imageURL})` }}>
             <img src={imageURL} alt="image" className="hidden" />
           </div>
--- src/pages/Phrase/Phrase.jsx
+++ src/pages/Phrase/Phrase.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ImageBorder from "../../components/ImageBorder/ImageBorder";
 import PhraseComponent from "../../components/PhraseComponent";
-import { cr7_3 } from "../../utils/image";
+import { cr7_8 } from "../../utils/image";
 
 const Phrase = () => {
   return (
@@ -12,7 +12,7 @@ const Phrase = () => {
         }
         isPhrase
       />
-      <ImageBorder imageURL={cr7_3} />
+      <ImageBorder imageURL={cr7_8} />
     </>
   );
 };
